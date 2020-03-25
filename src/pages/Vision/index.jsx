c5c5b185c38d2976e3be3fa2e60ce9d888df952f
Luis <avatar8492@gmail.com>
2020-03-24 22:30:03 -0600

    Link Header
    Se corrigieron errores del Header y Footer

--- src/pages/Vision/index.jsx
+++ src/pages/Vision/index.jsx
@@ -3,7 +3,7 @@ import Card from '../../components/Card';
 import './style.css';
 
 const title = 'Visión';
-const content= 'Gestionar el desarrollo informático de la Facultad de Ingeniería a través de procesos articulados de vanguardia, con esquemas de colaboración y alianzas sinérgicas al interior y exterior, promoviendo un manejo ágil, seguro, de mayores capacidades del conocimiento y de la informacion; emprendimiento y la vinculación, por medio de proyectos de innovación científicos y tecnológicos, a través deñ amplio acceso, uso y apropiación de las Tecnologías de la Infomación y la Comunicación TIC y de las Tecnologías de Aprendizaje y del Conocimineto TAC';
+const content= 'Gestionar el desarrollo informático de la Facultad de Ingeniería a través de procesos articulados de vanguardia, con esquemas de colaboración y alianzas sinérgicas al interior y exterior, promoviendo un manejo ágil, seguro, de mayores capacidades del conocimiento y de la informacion; emprendimiento y la vinculación, por medio de proyectos de innovación científicos y tecnológicos, a través deñ amplio acceso, uso y apropiación de las Tecnologías de la Infomación y la Comunicación TIC y de las Tecnologías de Aprendizaje y del Conocimineto TAC.';
 
 class Vision extends Component {
     render() {
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Historia from './pages/Historia';
 import Mision from './pages/Mision';
 import Vision from './pages/Vision';
 
 const Routes = () => (
-    <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Home}/>
             <Route exact path='/Historia' component={Historia} />
             <Route exact path='/Mision' component={Mision} />
             <Route exact path='/Vision' component={Vision} />
         </Switch>
-    </BrowserRouter>
 );
 
 export default Routes;
