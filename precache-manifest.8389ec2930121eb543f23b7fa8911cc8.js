0d62784a72e0bc6f31032de54c7806cc62183f91
Luis <avatar8492@gmail.com>
2020-04-12 22:12:33 -0500

    Updates

---
+++ precache-manifest.8389ec2930121eb543f23b7fa8911cc8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e510239e49b0158023e2e32243045e54",
+    "url": "/pagina-web/index.html"
+  },
+  {
+    "revision": "589ca1163d97249eee53",
+    "url": "/pagina-web/static/css/main.c020ea21.chunk.css"
+  },
+  {
+    "revision": "604d7700e6766105985b",
+    "url": "/pagina-web/static/js/2.5e4099a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pagina-web/static/js/2.5e4099a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589ca1163d97249eee53",
+    "url": "/pagina-web/static/js/main.87eb1360.chunk.js"
+  },
+  {
+    "revision": "58ace9fbb05be1f44953",
+    "url": "/pagina-web/static/js/runtime-main.9a783baa.js"
+  },
+  {
+    "revision": "8b2193244e4a11b5d27ea7e1b8323af9",
+    "url": "/pagina-web/static/media/EscudoFIVectorizadAzul2008PNG7x5.8b219324.png"
+  },
+  {
+    "revision": "8caa89bc328b409eb134b4b0358b0400",
+    "url": "/pagina-web/static/media/FI.8caa89bc.jpg"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/pagina-web/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/pagina-web/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "31400d0dd1835d32431ceecee4d0aa21",
+    "url": "/pagina-web/static/media/UNAM.31400d0d.png"
+  },
+  {
+    "revision": "4eee5db20b4a54b5e8eb8dc54ba7a0a2",
+    "url": "/pagina-web/static/media/escudounam_azul_m2008_png.4eee5db2.png"
+  },
+  {
+    "revision": "0cf3125a57948a32b2061bf97fd0006f",
+    "url": "/pagina-web/static/media/logo_azul_dorado.0cf3125a.png"
+  },
+  {
+    "revision": "2c3784c0638b2e4639f4c4e9bfc139e3",
+    "url": "/pagina-web/static/media/logo_dorado.2c3784c0.png"
+  }
+]);
